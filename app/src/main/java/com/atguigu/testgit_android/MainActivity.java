126b57bb076a6c8622cfac121203e2ed1dfb86c2
yangguangfu <trygf521@126.com>
2016-05-20 00:27:47 +0800

    第三版本要发布了哦

--- app/src/main/java/com/atguigu/testgit_android/MainActivity.java
+++ app/src/main/java/com/atguigu/testgit_android/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("第二版要发布了哦");
+        System.out.println("第三版要发布了哦");
     }
 }
